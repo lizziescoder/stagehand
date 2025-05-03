842d5dd37c6d846db3c2297da40ac3d8f7548c44
Anirudh Kamath <github@kamath.io>
2025-05-02 22:06:58 -0700

    greptile

--- examples/operator-example.ts
+++ examples/operator-example.ts
@@ -1,7 +1,7 @@
 /**
  * This example shows how to use the Stagehand operator to do simple autonomous tasks.
  *
- * This is built off of our open source project, Open Operator: operator.browserbase.com
+ * This is built off of our open source project, Open Operator: https://operator.browserbase.com
  *
  * To learn more about Stagehand Agents, see: https://docs.stagehand.dev/concepts/agent
  */
